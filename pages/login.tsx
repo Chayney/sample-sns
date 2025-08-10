be17eb312e9d1c180eb580a03ff9ee1f64abe91d
Chayney <chunchunmaru33333@gmail.com>
2025-08-10 13:15:47 +0900

    ログイン追加

---
+++ pages/login.tsx
@@ -0,0 +1,15 @@
+import LoginForm from "../components/loginForm";
+import Image from "next/image";
+
+export default function Page() {
+    return (
+        <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0">
+            <div>
+                <h1>ログイン</h1>
+            </div>
+            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg">
+                <LoginForm />
+            </div>
+        </div>
+    );
+}
